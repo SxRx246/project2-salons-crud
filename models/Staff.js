90bf246c0cfdcd736a029c64554597342af53047
Sara Alqallaf <202107125@stu.uob.edu.bh>
2025-07-31 13:55:36 +0300

    deleting service is working

---
+++ models/Staff.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const staffSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true, "name is required" ],
+    },
+    // we can make it as a range in the future
+    speciality:{
+        type:String,
+        required:[true, "speciality is required"]
+    }
+    ,
+    yearsOfExperience:{
+        type:Number,
+        required:true
+    }
+    ,
+    salon:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Salon",
+        required:true
+    }
+
+})
+
+const Staff = mongoose.model("Service",staffSchema)
+
+module.exports = Staff
