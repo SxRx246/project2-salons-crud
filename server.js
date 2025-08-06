5f5a34ce7d4658ee6fe22d10d92528ac7beb21fc
Sara Alqallaf <202107125@stu.uob.edu.bh>
2025-08-06 03:25:11 +0300

    clean server page and salons pages and added scripts partial to them

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-// imports
-const express = require("express") //importing express package
-const app = express() // creates a express application
-const dotenv = require("dotenv").config() //this allows me to use my .env values in this file
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv").config()
 const morgan = require("morgan")
 const methodOverride = require("method-override")
 const conntectToDB = require('./config/db')
@@ -14,45 +13,30 @@ const passUserToView = require('./middleware/passUserToView')
 const isSignedIn = require("./middleware/isSignedIn")
 
 
-
-
-// Middleware
-app.use(express.static('public')); //all static files are in the public folder
-app.use(express.urlencoded({ extended: false })); // this will allow us to see the data being sent in the POST or PUT
-app.use(methodOverride("_method")); // Changes the method based on the ?_method
-app.use(morgan("dev")) // logs the requests as they are sent to our sever in the terminal
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"))
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
   })
-); // uses the secret session code in the .env to encrypt the token
-app.use(passUserToView) //used to set the res.locals.user for each ejs page
-app.set("view engine", "ejs") //is more specific on which view engine we are using
-// app.set('views', path.join(__dirname, 'views')); 
+);
+app.use(passUserToView)
+app.set("view engine", "ejs")
 
-// connect to database
 conntectToDB()
 
 
 
+app.use("/auth", authRoutes)
+app.use(isSignedIn)
 
 
-
-
-
-app.use("/auth",authRoutes)
-app.use(isSignedIn) //all your protected routes go below this middleware
-// Routes go here
-
-app.use("/salons",salonRoutes)
-app.use("/appointments",appointmentRoutes)
-
-
-
-
-
+app.use("/salons", salonRoutes)
+app.use("/appointments", appointmentRoutes)
 
 
 
@@ -60,13 +44,13 @@ app.use("/appointments",appointmentRoutes)
 
 const port = process.env.PORT || 3000
 
-const server = app.listen(port,()=>{
-    console.log("Listening on port " + port)
-}) // Listen on port 3000
+const server = app.listen(port, () => {
+  console.log("Listening on port " + port)
+})
 
 
 
-server.on("error", (err) => { //console.log() an error if port 3000 is in use
+server.on("error", (err) => {
   if (err.code === "EADDRINUSE") {
     console.error(` Port ${port} is already in use.`);
   } else {
